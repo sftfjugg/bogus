304586707c82d744226a0dbe4cd0f4ab5cb8f6aa
Fan Yang <me@minidump.info>
2019-12-19 10:38:15 +0800

    work in progress

--- bogus.go
+++ bogus.go
@@ -1,8 +1,8 @@
-package dump
+package bogus
 
 import (
 	"context"
-	"fmt"
+	"net"
 
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
@@ -13,29 +13,24 @@ import (
 
 // N implements the plugin interface.
 type N struct {
-	Next  plugin.Handler
-	names []string
+	Next plugin.Handler
+	ips  []net.IP
 }
 
 func init() { plugin.Register("bogus", setup) }
 
 func setup(c *caddy.Controller) error {
-	names := []string{}
+	ips := []net.IP{}
 	for c.Next() {
 		args := c.RemainingArgs()
-		if len(args) == 0 {
-//			origins := make([]string, len(c.ServerBlockKeys))
-			copy(names, c.ServerBlockKeys)
-			continue
-		}
-		// I'll bet these are not fully qualified.
+
 		for _, a := range args {
-			names = append(names, dns.Fqdn(a))
+			ips = append(ips, net.ParseIP(a))
 		}
 	}
 
 	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
-		return N{Next: next, names: names}
+		return N{Next: next, ips: ips}
 	})
 
 	return nil
@@ -43,23 +38,13 @@ func setup(c *caddy.Controller) error {
 
 // ServeDNS implements the plugin.Handler interface.
 func (n N) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
-	for _, n := range n.names {
-		if dns.IsSubDomain(n, r.Question[0].Name) {
-			m := new(dns.Msg)
-			m.SetRcode(r, dns.RcodeNameError)
-			m.Ns = []dns.RR{soa(n)}
-			w.WriteMsg(m)
-			return 0, nil
-		}
+	wr := NewResponseReverter(w, r, n.ips)
+
+	if len(n.ips) == 0 {
+		return plugin.NextOrFailure(n.Name(), n.Next, ctx, w, r)
 	}
-	return plugin.NextOrFailure(n.Name(), n.Next, ctx, w, r)
+	return plugin.NextOrFailure(n.Name(), n.Next, ctx, wr, r)
 }
 
 // Name implements the Handler interface.
 func (n N) Name() string { return "bogus" }
-
-func soa(name string) dns.RR {
-	s := fmt.Sprintf("%s 60 IN SOA ns1.%s postmaster.%s 1524370381 14400 3600 604800 60", name, name, name)
-	soa, _ := dns.NewRR(s)
-	return soa
-}
---
+++ reverter.go
@@ -0,0 +1,72 @@
+package bogus
+
+import (
+	"fmt"
+	"net"
+	"strings"
+
+	"github.com/miekg/dns"
+)
+
+// ResponseReverter reverses the operations done on the question section of a packet.
+// This is need because the client will otherwise disregards the response, i.e.
+// dig will complain with ';; Question section mismatch: got example.org/HINFO/IN'
+type ResponseReverter struct {
+	dns.ResponseWriter
+	originalQuestion dns.Question
+	bogus            []net.IP
+}
+
+// NewResponseReverter returns a pointer to a new ResponseReverter.
+func NewResponseReverter(w dns.ResponseWriter, r *dns.Msg, bogus []net.IP) *ResponseReverter {
+	return &ResponseReverter{
+		ResponseWriter:   w,
+		originalQuestion: r.Question[0],
+		bogus:            bogus,
+	}
+}
+
+// WriteMsg records the status code and calls the underlying ResponseWriter's WriteMsg method.
+func (r *ResponseReverter) WriteMsg(res *dns.Msg) error {
+	res.Question[0] = r.originalQuestion
+	for _, rr := range res.Answer {
+		if rr.Header().Rrtype != dns.TypeA && rr.Header().Rrtype != dns.TypeAAAA {
+			continue
+		}
+
+		ss := strings.Split(rr.String(), "\t")
+		if len(ss) != 5 {
+			continue
+		}
+		ip := net.ParseIP(ss[4])
+		for _, i := range r.bogus {
+			if !ip.Equal(i) {
+				continue
+			}
+			rs := &dns.Msg{
+				MsgHdr: dns.MsgHdr{
+					Id:                 res.Id,
+					Response:           true,
+					RecursionDesired:   true,
+					RecursionAvailable: true,
+				},
+				Question: res.Question,
+				Answer:   []dns.RR{soa(r.originalQuestion.Name)},
+			}
+			return r.ResponseWriter.WriteMsg(rs)
+		}
+	}
+	return r.ResponseWriter.WriteMsg(res)
+}
+
+// Write is a wrapper that records the size of the message that gets written.
+func (r *ResponseReverter) Write(buf []byte) (int, error) {
+	n, err := r.ResponseWriter.Write(buf)
+	return n, err
+}
+
+func soa(name string) dns.RR {
+	s := fmt.Sprintf("%s 60 IN SOA ns1.%s postmaster.%s 1524370381 14400 3600 604800 60", name, name, name)
+	soa, _ := dns.NewRR(s)
+	return soa
+}
