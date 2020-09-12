138b21578f64ea587f63f2877b9ebd3c2d544356
Fan Yang <missdeer@dfordsoft.com>
2020-09-12 10:30:27 +0800

    (-)remove legacy metrics

--- bogus.go
+++ bogus.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
-	"github.com/coredns/coredns/plugin/metrics"
 
 	"github.com/caddyserver/caddy"
 	"github.com/miekg/dns"
@@ -35,7 +34,6 @@ func setup(c *caddy.Controller) error {
 	})
 
 	c.OnStartup(func() error {
-		metrics.MustRegister(c, HitsCount)
 		return nil
 	})
 	return nil
